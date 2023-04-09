b06b72ada66cc2395f8486b4590bac5b721d44b4
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-04-09 12:28:31 +0600

    Skeleton UI added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<h3 class="text-left">CodeShare</h3>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	// import CodeSnippetCard from '../CodeSnippetCard.svelte';
+	// import { snippetStore, addSnippet } from '../SnippetStore';
+	// import type { PageData } from './$types';
+	// export let data: PageData;
+
+	// let formData: CodeSnippetInput = {
+	// 	title: '',
+	// 	language: 'html',
+	// 	code: ''
+	// };
+
+	// snippetStore.set(data.snippets);
+</script>
+
+<div class="flex justify-center">
+	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+		<h3 class="text-center py-6">Create A Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y-4">
+			<label class="label">
+				<span>Snippet Title</span>
+				<input class="input" type="text" placeholder="Enter title here..." />
+			</label>
+			<label class="label">
+				<span>Programming Language</span>
+				<select class="select">
+					<option value="html">HTML</option>
+					<option value="css">CSS</option>
+					<option value="typescript">TypeScript</option>
+				</select>
+			</label>
+			<label class="label">
+				<span>Code Snippet</span>
+				<textarea class="textarea" rows="4" placeholder="Enter your snippet code here..." />
+			</label>
+			<button type="button" class="btn btn-sm variant-filled-primary">Create Snippet</button>
+		</div>
+		<div class="text-center py-6">
+			<h2>My Code Snippets</h2>
+		</div>
+	</div>
+</div>
